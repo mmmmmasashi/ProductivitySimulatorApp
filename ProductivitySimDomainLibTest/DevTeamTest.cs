45c6311330b2fcc8f27c3f5e36b7855c0053ff5d
m_masashi <drift_tms@yahoo.co.jp>
2022-09-12 08:14:20 +0900

    処理速度が小数点のケースのテストを実装

--- ProductivitySimDomainLibTest/DevTeamTest.cs
+++ ProductivitySimDomainLibTest/DevTeamTest.cs
@@ -3,11 +3,11 @@ using ProductivitySimDomainLib.Unit;
 
 namespace ProductivitySimDomainLibTest
 {
-    public class DevTeamTest
+    public class DevTeam_単位時間あたり処理量が3のTest
     {
         IUnit team;
 
-        public DevTeamTest()
+        public DevTeam_単位時間あたり処理量が3のTest()
         {
             const decimal TaskPerTime = 3;
             team = new DevTeam(TaskPerTime);
@@ -36,5 +36,46 @@ namespace ProductivitySimDomainLibTest
 
             Assert.Equal(2, outputs.Count);
         }
+    }
+
+    public class DevTeam_単位時間あたり処理量が0dot3のTest
+    {
+        IUnit team;
+
+        public DevTeam_単位時間あたり処理量が0dot3のTest()
+        {
+            const decimal TaskPerTime = 0.3M;
+            team = new DevTeam(TaskPerTime);
+        }
+
+        [Fact]
+        public void 単位時間あたり0dot3処理できるチームは4単位時間経過して1のアウトプットを出す()
+        {
+            //十分なタスクを与えておく
+            team.Input(new FeatureTask());
+            team.Input(new FeatureTask());
+
+            List<ITask> outputs1 = team.NextOutput();//0.3
+            Assert.Empty(outputs1);
+
+            List<ITask> outputs2 = team.NextOutput();//0.6
+            Assert.Empty(outputs2);
+
+            List<ITask> outputs3 = team.NextOutput();//0.9
+            Assert.Empty(outputs3);
+
+            List<ITask> outputs4 = team.NextOutput();//1.2 -> 0.2
+            Assert.Single(outputs4);
+
+            List<ITask> outputs5 = team.NextOutput();//0.5
+            Assert.Empty(outputs5);
+
+            List<ITask> outputs6 = team.NextOutput();//0.8
+            Assert.Empty(outputs6);
+
+            List<ITask> outputs7 = team.NextOutput();//1.1 -> 0.1
+            Assert.Single(outputs7);
+        }
+
     }
 }
